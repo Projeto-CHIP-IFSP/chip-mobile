4848c6b482a9e4663abfb919fed600e1bfc420b2
Ferreira06 <bielffonseca63@gmail.com>
2026-06-17 17:37:16 -0300

    fix: small fixes on camera work

--- app/(cam-teste)/cam-teste.tsx
+++ app/(cam-teste)/cam-teste.tsx
@@ -1,53 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { theme } from '../../src/constants/theme';
-import { Button } from '../../src/components/Button';
-import { Camera, useCameraDevice, useCameraPermission} from 'react-native-vision-camera';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
 
 export default function CamTeste() {
-  const camUser = useCameraDevice('front');
-  const permUser = useCameraPermission();
+  const { hasPermission, requestPermission } = useCameraPermission();
+  const device = useCameraDevice('front');
 
-  if (!permUser.hasPermission) {
-    return (
-      <View style={styles.content}>
-        <Text style={styles.subtitle}>Precisamos da permissão da câmera!</Text>
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission]);
 
-        <Button 
-          title="Permitir" 
-          onPress={permUser.requestPermission} 
-        />
-      </View>
-    );
-  }
-
-  if (camUser == null) {
-    return (
-      <View style={styles.content}>
-        <Text>Carregando câmera...</Text>
-      </View>
-    );
-  }
+  if (!hasPermission) return <Text style={styles.text}>Solicitando permissão de câmera...</Text>;
+  if (device == null) return <Text style={styles.text}>Nenhuma câmera frontal encontrada.</Text>;
 
   return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={camUser}
-      isActive={true}
-    />
+    <View style={styles.container}>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  content: {
+  container: {
     flex: 1,
+    backgroundColor: '#000',
     justifyContent: 'center',
-    paddingHorizontal: 24,
+    alignItems: 'center',
   },
-  subtitle: {
-    fontFamily: theme.fonts.regular,
+  text: {
+    color: 'white',
     fontSize: 16,
-    color: theme.colors.accent,
-    textAlign: 'center',
   }
 });
